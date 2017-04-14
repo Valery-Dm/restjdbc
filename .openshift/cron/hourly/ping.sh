1a9ad84f4be466b7e8d0fd92890553ebe221651c
user <dmitriev.valery@gmail.com>
2017-04-14 18:15:57 +0300

    cron hourly job left for testing

--- .openshift/cron/hourly/ping.sh
+++ .openshift/cron/hourly/ping.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 PATH=/bin:/usr/bin:/usr/sbin
-app_url=http://$OPENSHIFT_APP_DNS/
+#app_url=http://$OPENSHIFT_APP_DNS/
 
 #curl --insecure --location --silent --fail "$app_url" >/dev/null
 
+
+cd $OPENSHIFT_REPO_DIR/.openshift/action_hooks
+
+stop 
+&& sleep 3m && 
+start
+&& sleep 3m && 
 curl -X GET --header 'Accept: application/json' --header 'api_key: apiKey' 'http://restjdbc-va1ery.rhcloud.com:80/rest/roles/ADM' >> ${OPENSHIFT_DIY_LOG_DIR}/cron_hourly.log
--- .openshift/cron/minutely/tickle.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-date >> ${OPENSHIFT_DIY_LOG_DIR}/cron_minutely.log 
