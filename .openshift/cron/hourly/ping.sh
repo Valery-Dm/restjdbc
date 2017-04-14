2c3d67c1ce44dc50d6114c60cea568386c81f493
user <dmitriev.valery@gmail.com>
2017-04-14 17:56:24 +0300

    test cron jobs adjusted

--- .openshift/cron/hourly/ping.sh
+++ .openshift/cron/hourly/ping.sh
@@ -5,4 +5,4 @@ app_url=http://$OPENSHIFT_APP_DNS/
 
 #curl --insecure --location --silent --fail "$app_url" >/dev/null
 
-curl -X GET --header 'Accept: application/json' --header 'api_key: apiKey' 'http://restjdbc-va1ery.rhcloud.com:80/rest/roles/ADM' >> ${OPENSHIFT_DIY_LOG_DIR}/cron_hourly.log 
+curl -X GET --header 'Accept: application/json' --header 'api_key: apiKey' 'http://restjdbc-va1ery.rhcloud.com:80/rest/roles/ADM' >> ${OPENSHIFT_DIY_LOG_DIR}/cron_hourly.log
--- .openshift/cron/minutely/tickle.sh
+++ .openshift/cron/minutely/tickle.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-source $OPENSHIFT_CARTRIDGE_SDK_BASH
-
-set -x
-
-export JAVA_HOME=/etc/alternatives/java_sdk_1.8.0
-export PATH=$JAVA_HOME/bin:$PATH
 
 date >> ${OPENSHIFT_DIY_LOG_DIR}/cron_minutely.log 
