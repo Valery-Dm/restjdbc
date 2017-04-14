3c86d220c4741674fc91176a00a92ba84339372f
user <dmitriev.valery@gmail.com>
2017-04-14 17:26:57 +0300

    test cron jobs adjusted

--- .openshift/cron/minutely/tickle.sh
+++ .openshift/cron/minutely/tickle.sh
@@ -7,4 +7,4 @@ set -x
 export JAVA_HOME=/etc/alternatives/java_sdk_1.8.0
 export PATH=$JAVA_HOME/bin:$PATH
 
-date >> ${OPENSHIFT_PHP_LOG_DIR}/ticktock.log
+date >> ${OPENSHIFT_DIY_LOG_DIR}/cron_minutely.log 
