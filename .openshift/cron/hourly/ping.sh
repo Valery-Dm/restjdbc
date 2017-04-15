b89c064cc308ea242d96beb5b49a122c47383adf
user <dmitriev.valery@gmail.com>
2017-04-15 21:54:42 +0300

    cron hourly modified to return authorized answer. Need it for logs

--- .openshift/cron/hourly/ping.sh
+++ .openshift/cron/hourly/ping.sh
@@ -6,4 +6,4 @@ PATH=/bin:/usr/bin:/usr/sbin
 #curl --insecure --location --silent --fail "$app_url" >/dev/null
 
 
-curl -X GET --header 'Accept: application/json' --header 'api_key: apiKey' 'http://restjdbc-va1ery.rhcloud.com:80/rest/roles/ADM' >> ${OPENSHIFT_DIY_LOG_DIR}/cron_hourly.log
+curl -u 'demo.admin@spring.demo:123456' -X GET --header 'Accept: application/json' --header 'api_key: apiKey' 'http://restjdbc-va1ery.rhcloud.com:80/rest/roles/ADM' >> ${OPENSHIFT_DIY_LOG_DIR}/cron_hourly.log
