5f9fc2b282d20eccab9a73b3846b0d112d45ee25
user <dmitriev.valery@gmail.com>
2017-04-16 12:09:03 +0300

    set Tomcat Config for Deafault profile

--- src/main/java/dmv/spring/demo/security/TomcatHttpsConfig.java
+++ src/main/java/dmv/spring/demo/security/TomcatHttpsConfig.java
@@ -4,27 +4,32 @@ import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 
 /**
  * Configuring embedded Tomcat server to work through HTTPS
  *
  * @author dmv
  */
-//@Configuration
+@Configuration
+@Profile("default")
 public class TomcatHttpsConfig {
+    
+    private final Logger logger = LoggerFactory.getLogger(TomcatHttpsConfig.class);
 
 	@Bean
+	@Lazy
 	public EmbeddedServletContainerFactory servletContainer() {
 
 		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
 
 			@Override
 			protected void postProcessContext(Context context) {
-
+			    logger.debug("========================================= add CONFIDENTIAL for Default profile");
 				SecurityConstraint securityConstraint = new SecurityConstraint();
 				securityConstraint.setUserConstraint("CONFIDENTIAL");
 				SecurityCollection collection = new SecurityCollection();
@@ -40,7 +45,7 @@ public class TomcatHttpsConfig {
 	}
 
 	private Connector initiateHttpConnector() {
-
+	    logger.debug("========================================= add redirection to HTTPS for Default profile");
 		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
 		connector.setScheme("http");
 		connector.setPort(8080);
