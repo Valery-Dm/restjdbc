35436b33014b4427f1799b05f42ef58f67cae1a9
user <dmitriev.valery@gmail.com>
2017-04-16 18:41:13 +0300

    bash scripts re-organized

--- .openshift/cron/daily/restart.sh
+++ .openshift/cron/daily/restart.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-PATH=/bin:/usr/bin:/usr/sbin
-#app_url=http://$OPENSHIFT_APP_DNS/
+cd $OPENSHIFT_REPO_DIR/.openshift
 
-#curl --insecure --location --silent --fail "$app_url" >/dev/null
-
+source bash_init.sh
 
 cd $OPENSHIFT_REPO_DIR/.openshift/action_hooks
 
-source stop;
-sleep 3m;
-source start;
-sleep 3m;
-curl -X GET --header 'Accept: application/json' --header 'api_key: apiKey' 'http://restjdbc-va1ery.rhcloud.com:80/rest/roles/ADM' >> ${OPENSHIFT_DIY_LOG_DIR}/cron_daily.log
+source stop
+sleep 3m
+source start
+sleep 3m
+
+cd $OPENSHIFT_REPO_DIR
+
+curl -X GET --header 'Accept: application/json' 'http://restjdbc-va1ery.rhcloud.com:80/rest/roles/ADM' >> ${OPENSHIFT_DIY_LOG_DIR}/cron_daily.log
--- .openshift/cron/hourly/ping.sh
+++ .openshift/cron/hourly/ping.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-PATH=/bin:/usr/bin:/usr/sbin
-#app_url=http://$OPENSHIFT_APP_DNS/
+cd $OPENSHIFT_REPO_DIR/.openshift
 
-#curl --insecure --location --silent --fail "$app_url" >/dev/null
+source bash_init.sh
 
-
-curl -u 'demo.admin@spring.demo:123456' -X GET --header 'Accept: application/json' --header 'api_key: apiKey' 'http://restjdbc-va1ery.rhcloud.com:80/rest/roles/ADM' >> ${OPENSHIFT_DIY_LOG_DIR}/cron_hourly.log
+curl -u 'demo.admin@spring.demo:123456' -X GET --header 'Accept: application/json' 'http://restjdbc-va1ery.rhcloud.com:80/rest/roles/ADM' >> ${OPENSHIFT_DIY_LOG_DIR}/cron_hourly.log
