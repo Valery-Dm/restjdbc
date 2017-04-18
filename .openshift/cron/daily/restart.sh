818f21cc813ae9655763c7c0a74410a8b418d2b2
user <dmitriev.valery@gmail.com>
2017-04-18 14:28:38 +0300

    scheduled jobs minor changes

--- .openshift/cron/daily/restart.sh
+++ .openshift/cron/daily/restart.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 PATH=/bin:/usr/bin:/usr/sbin
-
 cd $OPENSHIFT_REPO_DIR/.openshift/action_hooks
 
-source stop
-sleep 3m
-source start
-sleep 3m
-curl -X GET --header 'Accept: application/json' 'http://restjdbc-va1ery.rhcloud.com:80/rest/roles/ADM' >> ${OPENSHIFT_DIY_LOG_DIR}/cron_daily.log
+echo stopping...
+source stop 2>&1
+sleep 1m
+echo starting...
+source start 2>&1
+sleep 1m
+echo testing...
+curl -u 'demo.admin@spring.demo:123456' -X GET --header 'Accept: application/json' 'http://restjdbc-va1ery.rhcloud.com:80/rest/roles/ADM' >> ${OPENSHIFT_DIY_LOG_DIR}/cron_daily.log &
+
--- .openshift/cron/hourly/ping.sh
+++ .openshift/cron/hourly/ping.sh
@@ -2,4 +2,4 @@
 
 PATH=/bin:/usr/bin:/usr/sbin
 
-curl -u 'demo.admin@spring.demo:123456' -X GET --header 'Accept: application/json' 'http://restjdbc-va1ery.rhcloud.com:80/rest/roles/ADM' >> ${OPENSHIFT_DIY_LOG_DIR}/cron_hourly.log
+curl -u 'demo.admin@spring.demo:123456' -X GET --header 'Accept: application/json' 'http://restjdbc-va1ery.rhcloud.com:80/rest/roles/ADM' >> ${OPENSHIFT_DIY_LOG_DIR}/cron_hourly.log &
